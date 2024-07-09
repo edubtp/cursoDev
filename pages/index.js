dd6ac82de16c4f061e8cb46950dfb2b7227c4618
Jose eduardo da silva <jose.eduardo@sulamerica.com.br>
2024-07-08 21:58:54 -0300

    Adicionando o lint:check

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
 // pages/index.js
 const Home = () => {
-    const handleLogin = (event) => {
-        event.preventDefault(); // Evita que o formulário recarregue a página
-        // Aqui você pode adicionar lógica para autenticar o usuário
-        alert('Login realizado!');
-    };
+  const handleLogin = (event) => {
+    event.preventDefault(); // Evita que o formulário recarregue a página
+    // Aqui você pode adicionar lógica para autenticar o usuário
+    alert("Login realizado!");
+  };
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <label>
-                    Username:
-                    <input type="text" />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" />
-                </label>
-                <br />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
